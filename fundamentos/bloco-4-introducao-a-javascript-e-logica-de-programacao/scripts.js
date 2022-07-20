81f8ba4b28feafdb34ff99a641508176de7e62e2
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-07-20 16:02:45 -0300

    Criação do bloco-4-introducao-a-javascript-e-logica-de-programacao

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/scripts.js
@@ -0,0 +1,12 @@
+const myName = "Joseane Oliveira"
+
+const birthCity = "São José dos campos - SP"
+
+birthCity = "São Paulo"
+
+let birthYear = 1997
+birthYear =2030
+
+
+console.log(birthYear)
+console.log(birthCity)
