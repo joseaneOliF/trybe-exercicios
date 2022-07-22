fdeff4f6ea7521e2a1445978daa5a8f3f919f64a
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-07-22 11:51:45 -0300

    Incluindo forOf no arquivo for.js

--- fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia2/for.js
+++ fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia2/for.js
@@ -16,10 +16,14 @@
 //Exercício 1 Utilize o for para imprimir os elementos da lista groceryList com o console.log():
 
 
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+//let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 
-for (let index = 0; index < groceryList.length; index += 1)
-console.log(groceryList[index]);
+//for (let index = 0; index < groceryList.length; index += 1)
+//console.log(groceryList[index]);
 
+//for of 
 
-
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let eachName of names) { 
+console.log(eachName);
+}
