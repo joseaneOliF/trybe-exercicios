fcfe73e6b61529093546fba5a302f8407b123c98
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-07-31 13:31:47 -0300

    Refazendo alguns exercícios

--- fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia2/exercicio.js
+++ fundamentos/bloco-4-introducao-js-e-logica-de-programacao/dia2/exercicio.js
@@ -134,19 +134,18 @@
 //Ordene o array numbers em ordem crescente e imprima seus valores;
 //Solução:
 //
-//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-//
-//for (let index = 1; index < numbers.length; index += 1) {
-//  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-//    if (numbers[index] < numbers[secondIndex]) {
-//      let position = numbers[index];
-//      numbers[index] = numbers[secondIndex];
-//      numbers[secondIndex] = position;
-//    }
-//  }
-//}
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//console.log(numbers);
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+console.log(numbers);
 
 //Exercício 2
 
@@ -175,15 +174,15 @@
 //Agora você irá criar um novo array a partir do array numbers, sem perdê-lo. Cada valor do novo array deverá ser igual ao valor correspondente do array anterior multiplicado pelo próximo. Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (valor correspondente) e 9 (próximo valor). Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push.
 //Solução:
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let newArray = [];
-
-for (let index = 0; index < numbers.length; index += 1) {
-  if (index + 1 < numbers.length) {
-    newArray.push(numbers[index] * numbers[index + 1]);
-  } else {
-    newArray.push(numbers[index] * 2);
-  }
-}
-
-console.log(newArray);
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let newArray = [];
+//
+//for (let index = 0; index < numbers.length; index += 1) {
+//  if (index + 1 < numbers.length) {
+//    newArray.push(numbers[index] * numbers[index + 1]);
+//  } else {
+//    newArray.push(numbers[index] * 2);
+//  }
+//}
+//
+//console.log(newArray);
