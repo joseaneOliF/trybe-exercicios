3da19ab15145235e8ee92a87c5c359b3380f8e7a
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-08-12 19:05:59 -0300

    Exercício 1 dia-1

---
+++ fundamentos/bloco-7-introducao-javascriptes6-e-testes-unitarios/dia-1/Exercicio-1.js
@@ -0,0 +1,14 @@
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      console.log(`${ifScope} 'ótimo, fui utilizada no escopo`);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    //console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(false);
+
+  
---
+++ fundamentos/bloco-7-introducao-javascriptes6-e-testes-unitarios/dia-1/scriptfix.js
@@ -0,0 +1,19 @@
+// Com o template literals
+//console.log(`Primeira linha;
+//Segunda linha;
+//Terceira linha;`
+//);
+//
+//// Sem o template literals:
+//console.log('Primeira linha;\n' + 'Segunda linha;\n' + 'Terceira linha;\n');
+
+const nome = "Gus";
+const modulos = [1];
+const empresa = {
+  nome: "Trybe",
+  nota: 8001,
+};
+
+modulos.push(3);
+nome = "Batman";
+empresa.nota = 8002;
