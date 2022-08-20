d08b0ed67f4db097eb9274a3f9302538771fd999
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-08-19 22:00:45 -0300

    Exercício 3 dia 1 - bloco 8

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1/exercicio-3.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1/exercicio-3.js
@@ -10,3 +10,24 @@
 
 const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnswers = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+
+// Iniciaremos nossa HOF, ela receberá os mesmos parâmetros da callback mais a callback em si. Para contagem dos pontos teremos um contador que será alterado por um loop for que irá percorrer pelo array de respostas corretas. 
+const countPoints = (rightAnswers, studentAnswers, action) => {
+    let counter = 0;
+    for (let index = 0; index < rightAnswers.length; index += 1) {
+      const actionReturn = action(rightAnswers[index], studentAnswers[index]); // A cada iteração utilizaremos nossa callback. Como parâmetros utilizamos o index do loop para compararmos as respostas corretas com as do estudante.
+      counter += actionReturn;
+    }
+    return `Resultado final: ${counter} pontos`;
+  };
+  
+  console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
